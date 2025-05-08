fbb03f302c572e217784b10b9283ef562d4fe2cf
Lab5 <wathakate@gmail.com>
2025-05-08 08:35:01 -0300

    ignore

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,11 @@
 package Main;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList<File> test = RMMUtils.getFilesByType("pl","C:/Users/Lab5/Downloads/test/");
+
     }
 }
--- src/Main/RMMUtils.java
+++ src/Main/RMMUtils.java
@@ -1,30 +1,37 @@
 package Main;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class RMMUtils {
-    public ArrayList<File> getFilesByType(String type, String path){
+     public static ArrayList<File> getFilesByType(String type, String path){
         File dir = new File(path);
         ArrayList<File> files = new ArrayList<>();
 
         // Searches for files of type (pl, em, etc)
+        search(type,dir,files);
+
+        return files;
+    }
+    private static ArrayList<File> search(String type, File dir, ArrayList<File> list){
+        ArrayList<File> files = new ArrayList<>();
+        files.addAll(list);
         File[] filesTemp = dir.listFiles();
         for (File file : filesTemp) {
             if (file.isFile() && file.getName().contains(type)) {
                 files.add(file);
                 System.out.println(file);
+            } else if (file.isDirectory()) {
+                files.addAll(search(type,dir,files));
             }
         }
-
         return files;
     }
-    public String createProject(String path, String name,ArrayList<String> folders){
+    public static String createProject(String path, String name,ArrayList<String> folders){
 
         return path+"/"+name;
     }
-    public void createModini(String path,String author, String description){
+    public static void createModini(String path,String author, String description){
 
     }
 
