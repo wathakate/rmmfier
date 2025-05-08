04efc5dc1847bcfa126439c53cbded2bf296df57
Lab5 <wathakate@gmail.com>
2025-05-08 09:29:59 -0300

    ignore

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,24 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        File source = new File("C:/Users/Lab5/Downloads/test");
+        File[] filesTemp = source.listFiles();
+        ArrayList<String> types = new ArrayList<>();
+        for (File file : filesTemp) {
+            switch (file.getName()){
+                case "pl":
+                    types.add("pl");
+                    break;
+                case "em":
+                    types.add("em");
+                    break;
+                case "wp":
+                    types.add("wp");
+                    break;
+            }
+        }
         ArrayList<File> test = RMMUtils.getFilesByType("pl","C:/Users/Lab5/Downloads/test/");
+        RMMUtils.createProject("C:/Users/Lab5/Downloads/","test2", types);
 
     }
 }
--- src/Main/RMMUtils.java
+++ src/Main/RMMUtils.java
@@ -4,31 +4,34 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class RMMUtils {
-     public static ArrayList<File> getFilesByType(String type, String path){
+    public static ArrayList<File> getFilesByType(String type, String path){
+         //This function might be unnecessary but it is what it is
         File dir = new File(path);
         ArrayList<File> files = new ArrayList<>();
 
-        // Searches for files of type (pl, em, etc)
-        search(type,dir,files);
+        files.addAll(search(type,dir,files));
 
         return files;
     }
     private static ArrayList<File> search(String type, File dir, ArrayList<File> list){
-        ArrayList<File> files = new ArrayList<>();
-        files.addAll(list);
+        // Searches for files of type (pl, em, etc.)
+        ArrayList<File> files = new ArrayList<>(list);
         File[] filesTemp = dir.listFiles();
         for (File file : filesTemp) {
             if (file.isFile() && file.getName().contains(type)) {
                 files.add(file);
                 System.out.println(file);
             } else if (file.isDirectory()) {
-                files.addAll(search(type,dir,files));
+                files.addAll(search(type,file,files));
             }
         }
         return files;
     }
     public static String createProject(String path, String name,ArrayList<String> folders){
-
+        new File(path+"/"+name).mkdirs();
+        for (int i = 0; i < folders.size(); i++) {
+            new File(path+"/"+name+"/"+folders.get(i)).mkdirs();
+        }
         return path+"/"+name;
     }
     public static void createModini(String path,String author, String description){
