76d753f96eb423806a9811f71569708d07ae0410
Lab5 <wathakate@gmail.com>
2025-05-15 09:04:11 -0300

    what

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,28 +1,29 @@
 package Main;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         File source = new File("C:/Users/Lab5/Downloads/test");
-        File[] filesTemp = source.listFiles();
+
         ArrayList<String> types = new ArrayList<>();
-        for (File file : filesTemp) {
-            switch (file.getName()){
-                case "pl":
-                    types.add("pl");
-                    break;
-                case "em":
-                    types.add("em");
-                    break;
-                case "wp":
-                    types.add("wp");
-                    break;
-            }
+        if (!RMMUtils.getFilesByType("pl",source.getAbsolutePath()).isEmpty()){
+            types.add("pl");
         }
-        ArrayList<File> test = RMMUtils.getFilesByType("pl","C:/Users/Lab5/Downloads/test/");
-        RMMUtils.createProject("C:/Users/Lab5/Downloads/","test2", types);
+        if (!RMMUtils.getFilesByType("em",source.getAbsolutePath()).isEmpty()){
+            types.add("em");
+        }
+        if (!RMMUtils.getFilesByType("wp",source.getAbsolutePath()).isEmpty()){
+            types.add("wp");
+        }
+
+        try {
+            RMMUtils.createProject("C:/Users/Lab5/Downloads/",source.getAbsolutePath(), "test2",types);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/Main/RMMUtils.java
+++ src/Main/RMMUtils.java
@@ -1,6 +1,9 @@
 package Main;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class RMMUtils {
@@ -20,19 +23,31 @@ public class RMMUtils {
         for (File file : filesTemp) {
             if (file.isFile() && file.getName().contains(type)) {
                 files.add(file);
-                System.out.println(file);
             } else if (file.isDirectory()) {
                 files.addAll(search(type,file,files));
             }
         }
         return files;
     }
-    public static String createProject(String path, String name,ArrayList<String> folders){
-        new File(path+"/"+name).mkdirs();
-        for (int i = 0; i < folders.size(); i++) {
-            new File(path+"/"+name+"/"+folders.get(i)).mkdirs();
+    private static void copyFiles(String type,String fromFolder,String toFolder) throws IOException {
+        File toWhere = new File(toFolder);
+        ArrayList<File> filesTemp = getFilesByType(type,fromFolder);
+        for (int i = 0; i < filesTemp.size();i++){
+            Files.copy(filesTemp.get(i).toPath(), toWhere.toPath());
+            System.out.println("copied "+filesTemp.get(i).getName());
         }
-        return path+"/"+name;
+    }
+    public static String createProject(String path, String originalFolder,String name,ArrayList<String> folders) throws IOException {
+        new File(path+"/"+name).mkdirs();
+        new File(path+"/"+name+"/data000").mkdirs();
+        for (int i = 0; i < folders.size(); i++) {
+            new File(path+"/"+name+"/data000/"+folders.get(i)).mkdirs();
+            System.out.println("made "+folders.get(i));
+            copyFiles(folders.get(i), originalFolder,path+"/"+name+"/data000/"+folders.get(i));
+            System.out.println("copied");
+        }
+
+        return path+"/"+name+"/data000";
     }
     public static void createModini(String path,String author, String description){
 
